6298359a52b255119e895cfe1349e7e65f885aa8
amichalo421 <andre.michalowski@gmail.com>
2020-10-30 13:36:20 -0400

    Actions: GET requests

--- Routers/actions/actionsRouter.js
+++ Routers/actions/actionsRouter.js
@@ -3,8 +3,37 @@ const router = express.Router(); //instantiate
 
 const actions = require('../../data/helpers/actionModel'); //import functions
 
-  // router.get('/', (req, res) => {...}
-  // router.get('/:id', (req, res) => {...}
+
+  //GET----------------------------------------------------------------//
+
+  //GET ACTIONS
+  router.get("/actions", (req, res) => {
+    actions
+      .get()
+      .then((action) => {
+        res.status(200).json(action);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "Error retrieving action.",
+        });
+      });
+  });
+  
+  //GET ACTION BY ID
+  router.get("/actions/:id", (req, res) => {
+    actions
+      .get(req.params.id)
+      .then((action) => {
+        res.status(200).json(action);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "Error retrieving action.",
+        });
+      });
+  });
+
   // router.post('/', (req, res) => {...}
   // router.delete('/:id', (req, res) => {...}
   // router.put('/:id', (req, res) => {...}
